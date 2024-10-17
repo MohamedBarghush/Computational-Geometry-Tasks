8ff5da3eeb445fa3a22c5194d69b15c915387e3a
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-18 01:36:11 +0300

    Implementing Extreme Points

--- CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -12,7 +12,30 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-           
+            // Find the extreme points by exterminating the points that are inside the triangle formed by any 3 points
+            HashSet<Point> extremePoints = new HashSet<Point>(points);
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (i == j) continue;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (i == k || j == k) continue;
+                        for (int l = 0; l < points.Count; l++)
+                        {
+                            if (i == l || j == l || k == l) continue;
+                            if (HelperMethods.PointInTriangle(points[i], points[j], points[k], points[l]) == Enums.PointInPolygon.Inside)
+                            {
+                                extremePoints.Remove(points[i]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            outPoints = extremePoints.ToList();
         }
 
         public override string ToString()
