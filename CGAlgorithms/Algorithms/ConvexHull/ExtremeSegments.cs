7825eb2f56d23777c0b60bb4f451341338df1954
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-18 02:21:47 +0300

    Implementing Extreme Segments

--- CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
@@ -11,6 +11,49 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            //for (int i = 0; i < points.Count; i++)
+            //{
+            //    Debug.Print(points[i].X + " " + points[i].Y);
+            //}
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (i == j) continue;
+
+                    bool allOnSameSide = true;
+                    Enums.TurnType? firstTurnType = null;
+
+                    // Adding lines
+                    //if (lines.Contains(new Line(points[i], points[j])) == false) lines.Add(new Line(points[i], points[j]));
+
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (j == k || i == k) continue;
+
+                        Point vectorIJ = new Point(points[j].X - points[i].X, points[j].Y - points[i].Y);
+                        Point vectorIK = new Point(points[k].X - points[i].X, points[k].Y - points[i].Y);
+                        
+                        Enums.TurnType turnType = HelperMethods.CheckTurn(vectorIJ, vectorIK);
+
+                        if (firstTurnType == null && turnType != Enums.TurnType.Colinear)
+                        {
+                            firstTurnType = turnType;
+                        }
+                        else if (turnType != Enums.TurnType.Colinear && turnType != firstTurnType)
+                        {
+                            allOnSameSide = false;
+                            break;
+                        }
+                    }
+
+                    if (allOnSameSide)
+                    {
+                        outLines.Add(new Line(points[i], points[j]));
+                    }
+                }
+            }
 
         }
 
