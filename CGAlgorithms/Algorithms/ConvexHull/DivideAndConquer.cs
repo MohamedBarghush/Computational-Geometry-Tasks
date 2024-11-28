aa86e76342e747027fc6d7cf9dc92ac1448d5e91
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2024-11-28 13:43:48 +0200

    D&C is DONE

--- CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -9,12 +9,83 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class DivideAndConquer : Algorithm
     {
-        public List<Point> divide(List<Point> points)
+
+
+
+        private List<Line> Merge(List<Point> leftHull, List<Point> rightHull)
+        {
+            int upperLeftIndex = 0;
+            int upperRightIndex = 0;
+
+            for (int i = 0; i < leftHull.Count; i++)
+            {
+                if (leftHull[i].Y > leftHull[upperLeftIndex].Y)
+                {
+                    upperLeftIndex = i;
+                }
+            }
+
+            for (int i = 0; i < rightHull.Count; i++)
+            {
+                if (rightHull[i].Y > rightHull[upperRightIndex].Y)
+                {
+                    upperRightIndex = i;
+                }
+            }
+
+            // to get the lower tangent
+            // we need to find the min Y point in the right hull and the min Y point in the left hull
+
+            int lowerLeftIndex = 0;
+            int lowerRightIndex = 0;
+
+            for (int i = 0; i < leftHull.Count; i++)
+            {
+                if (leftHull[i].Y < leftHull[lowerLeftIndex].Y)
+                {
+                    lowerLeftIndex = i;
+                }
+            }
+
+            for (int i = 0; i < rightHull.Count; i++)
+            {
+                if (rightHull[i].Y < rightHull[lowerRightIndex].Y)
+                {
+                    lowerRightIndex = i;
+                }
+            }
+
+            // Points on the upper tangent
+            Point upperLeftPoint = leftHull[upperLeftIndex];
+            Point upperRightPoint = rightHull[upperRightIndex];
+
+            // // Points on the lower tangent
+            Point lowerLeftPoint = leftHull[lowerLeftIndex];
+            Point lowerRightPoint = rightHull[lowerRightIndex];
+
+            List<Line> tangents = new List<Line>
+            {
+                new Line(upperLeftPoint, upperRightPoint),
+                new Line(lowerLeftPoint, lowerRightPoint)
+            };
+
+            return tangents;
+        }
+
+
+        private List<Point> SortPoints(List<Point> points)
+        {
+            // Use the centroid of the hull points to sort them in counterclockwise order
+            Point center = new Point(points.Average(p => p.X), points.Average(p => p.Y));
+            return points.OrderBy(p => Math.Atan2(p.Y - center.Y, p.X - center.X)).ToList();
+        }
+
+
+        private List<Point> ComputeConvexHull(List<Point> points)
         {
             if (points.Count < 3)
-            {
                 return points;
-            }
+
             List<Point> outPut = new List<Point>();
 
             Point a = points[0];
@@ -26,86 +97,92 @@ namespace CGAlgorithms.Algorithms.ConvexHull
                 outPut = points;
                 return outPut;
             }
-            List<Point> Rightside = new List<Point>();
-            List<Point> Left = new List<Point>();
+
+            // Divide the points into two subsets: above and below the line formed by leftmost and rightmost points
+            List<Point> RightSide = new List<Point>();
+            List<Point> LeftSide = new List<Point>();
+
             for (int i = points.Count / 2; i < points.Count; i++)
             {
-                Left.Add(points[i]);
+                LeftSide.Add(points[i]);
             }
 
             for (int i = 0; i < points.Count / 2; i++)
             {
-                Rightside.Add(points[i]);
+                RightSide.Add(points[i]);
             }
-            Left = divide(Left);
-            Rightside = divide(Rightside);
 
-            // MERGE
-            double max_Y_L = double.MinValue;
-            Point max_L = new Point(2, 3);
-            for (int i = 0; i < Left.Count; i++)
+            LeftSide = ComputeConvexHull(LeftSide);
+            RightSide = ComputeConvexHull(RightSide);
+
+            int upperLeftIndex = 0;
+            int upperRightIndex = 0;
+
+            int lowerLeftIndex = 0;
+            int lowerRightIndex = 0;
+
+
+            for (int i = 0; i < LeftSide.Count; i++)
             {
-                if (Left[i].Y > max_Y_L)
+                if (LeftSide[i].Y > LeftSide[upperLeftIndex].Y)
                 {
-                    max_Y_L = Left[i].Y;
-                    max_L = Left[i];
-                }
-            }
-            double max_Y_R = double.MinValue;
-            Point max_R = new Point(2, 3);
-            for (int i = 0; i < Rightside.Count; i++)
-            {
-                if (Rightside[i].Y > max_Y_R)
-                {
-                    max_Y_R = Rightside[i].Y;
-                    max_R = Rightside[i];
+                    upperLeftIndex = i;
                 }
             }
 
-            double min_Y_R = double.MaxValue;
-            Point min_R = new Point(2, 3);
-            for (int i = 0; i < Rightside.Count; i++)
+            for (int i = 0; i < RightSide.Count; i++)
             {
-                if (Rightside[i].Y < min_Y_R)
+                if (RightSide[i].Y > RightSide[upperRightIndex].Y)
                 {
-                    min_Y_R = Rightside[i].Y;
-                    min_R = Rightside[i];
-                }
-            }
-            double min_Y_Left = double.MaxValue;
-            Point min_Left = new Point(2, 3);
-            for (int i = 0; i < Left.Count; i++)
-            {
-                if (Left[i].Y < min_Y_Left)
-                {
-                    min_Y_Left = Left[i].Y;
-                    min_Left = Left[i];
+                    upperRightIndex = i;
                 }
             }
 
-            Line right = new Line(max_R, min_R);
-            for (int i = 0; i < Rightside.Count; i++)
-            {
-                Enums.TurnType cr = HelperMethods.CheckTurn(right, Rightside[i]);
-                if (cr == Enums.TurnType.Right || cr == Enums.TurnType.Colinear || Rightside[i].Y > max_L.Y || Rightside[i].Y < min_Left.Y)
-                    if (!outPut.Contains(Rightside[i]))
-                        outPut.Add(Rightside[i]);
 
+            for (int i = 0; i < LeftSide.Count; i++)
+            {
+                if (LeftSide[i].Y < LeftSide[lowerLeftIndex].Y)
+                {
+                    lowerLeftIndex = i;
+                }
             }
 
-            Line left = new Line(max_L, min_Left);
-            for (int i = 0; i < Left.Count; i++)
+            for (int i = 0; i < RightSide.Count; i++)
             {
-                Enums.TurnType cl = HelperMethods.CheckTurn(left, Left[i]);
+                if (RightSide[i].Y < RightSide[lowerRightIndex].Y)
+                {
+                    lowerRightIndex = i;
+                }
+            }
+
+            Point upperLeftPoint = LeftSide[upperLeftIndex];
+            Point upperRightPoint = RightSide[upperRightIndex];
+            Point lowerLeftPoint = LeftSide[lowerLeftIndex];
+            Point lowerRightPoint = RightSide[lowerRightIndex];
+
+            Line right = new Line(upperRightPoint, lowerRightPoint);
+            for (int i = 0; i < RightSide.Count; i++)
+            {
+                Enums.TurnType cr = HelperMethods.CheckTurn(right, RightSide[i]);
+                if (cr == Enums.TurnType.Right || cr == Enums.TurnType.Colinear || RightSide[i].Y > upperLeftPoint.Y || RightSide[i].Y < lowerLeftPoint.Y)
+                    if (!outPut.Contains(RightSide[i]))
+                        outPut.Add(RightSide[i]);
+            }
+
+            Line left = new Line(upperLeftPoint, lowerLeftPoint);
+            for (int i = 0; i < LeftSide.Count; i++)
+            {
+                Enums.TurnType cl = HelperMethods.CheckTurn(left, LeftSide[i]);
                 if (cl == Enums.TurnType.Left || cl == Enums.TurnType.Colinear)
-                    if (!outPut.Contains(Left[i]))
-                        outPut.Add(Left[i]);
+                    if (!outPut.Contains(LeftSide[i]))
+                        outPut.Add(LeftSide[i]);
             }
 
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             for (int i = 0; i < outPut.Count; i++)
             {
-                if (outPut[i].X < max_L.X && outPut[i].X > min_Left.X)
+                if (outPut[i].X < upperLeftPoint.X && outPut[i].X > lowerLeftPoint.X)
                     outPut.RemoveAt(i);
             }
             for (int i = 1; i < outPut.Count - 1; i++)
@@ -124,32 +201,45 @@ namespace CGAlgorithms.Algorithms.ConvexHull
                     outPut.RemoveAt(i);
             }
 
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
             return outPut;
         }
-        private List<Point> SortPoints(List<Point> points)
-        {
-            // Use the centroid of the hull points to sort them in counterclockwise order
-            Point center = new Point(points.Average(p => p.X), points.Average(p => p.Y));
-            return points.OrderBy(p => Math.Atan2(p.Y - center.Y, p.X - center.X)).ToList();
-        }
+
+
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-            if (points.Count <= 3)
+            // RemoveDuplicatePoints(ref points);
+
+            if (points.Count < 3)
             {
                 outPoints = points;
             }
             else
             {
                 points = points.OrderBy(xa => xa.X).ToList();
-                outPoints = divide(points);
-                outPoints = SortPoints(outPoints);
+
+
+                // Compute the convex hull for the left subset
+                List<Point> result = ComputeConvexHull(points);
+
+                outPoints = SortPoints(result);
+
+
+
                 for (int i = 0; i < outPoints.Count; i++)
                 {
                     Point start = outPoints[i];
-                    Point end = outPoints[(i + 1) % outPoints.Count]; 
+                    Point end = outPoints[(i + 1) % outPoints.Count]; // Wrap around to form a closed loop
                     outLines.Add(new Line(start, end));
                 }
             }
+
+
+
         }
 
         public override string ToString()
