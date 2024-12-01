d42e8e7fcbcf5c8657887fe1137a39beafb8800e
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2024-12-01 21:36:14 +0200

    commit message Co-authored-by: MohamedBarghush cinosmoh@gmail.com

--- CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -159,7 +159,7 @@ namespace CGAlgorithms.Algorithms.ConvexHull
             Point upperRightPoint = RightSide[upperRightIndex];
             Point lowerLeftPoint = LeftSide[lowerLeftIndex];
             Point lowerRightPoint = RightSide[lowerRightIndex];
-
+             //hello world 
             Line right = new Line(upperRightPoint, lowerRightPoint);
             for (int i = 0; i < RightSide.Count; i++)
             {
